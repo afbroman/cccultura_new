02e4a0a9068d14da04f6e61e57043bf3120a53db
Andrew Broman <broman@acm.org>
2012-12-24 16:33:50 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcculturaNew::Application.config.secret_token = 'f6fabea55d9aad2041c02aea4098ea783381aa27bf81f0db9df58aa3fe3c313aef7de1c84f471c936bab7ae109aff455086348bab17d1745021412322b2fa2b9'
