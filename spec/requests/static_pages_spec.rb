91e903d3ef6be610130ef0ccb74d6aeea4bb7720
Andrew Broman <broman@acm.org>
2012-12-24 17:12:55 -0600

    Add titles to static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,20 +7,24 @@ describe "Static Pages" do
   describe "About page" do
     before { visit "/static_pages/about" }
     it { should have_content 'Welcome to ccCultura' }
+    it { should have_selector('title', text: "ccCultura | About") }
   end
 
   describe "Follow page" do
     before { visit "/static_pages/follow" }
     it { should have_content 'Follow ccCultura' }
+    it { should have_selector('title', text: "ccCultura | Follow") }
   end
 
   describe "Contribute page" do
     before { visit "/static_pages/contribute" }
     it { should have_content 'ccContribute' }
+    it { should have_selector('title', text: "ccCultura | Contribute") }
   end
 
   describe "Links page" do
     before { visit "/static_pages/links" }
     it { should have_content "Links & Source Material" }
+    it { should have_selector('title', text: "ccCultura | Links") }
   end
 end
